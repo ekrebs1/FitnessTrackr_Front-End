0152fed3e99bb69996d781f0c0ff107b08fc8ee6
Erica <ericakrebs1@gmail.com>
2021-06-02 23:11:14 -0500

    create react app

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+const BASE = "https://obscure-beach-19902.herokuapp.com";
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+import ReactDOM from "react-dom";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
