ca3eee69cd38dfc7c4f74cbf2e359910a46cc20a
Erica <ericakrebs1@gmail.com>
2021-06-06 11:43:21 -0500

    routines

--- my-app/src/components/Routines.js
+++ my-app/src/components/Routines.js
@@ -1,7 +1,120 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
+// import { ReplyOutlined } from "@material-ui/icons";
 
 const Routines = () => {
-  return <div>This is the Routines page</div>;
+  const [routines, setRoutines] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_FITNESS_TRACKR_API_URL}routines`)
+      .then(({ data }) => {
+        if (data.length) {
+          setRoutines(data);
+        }
+      });
+  }, []);
+  return (
+    <>
+      <h1>Routines Page</h1>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align='right'>ID</TableCell>
+              <TableCell align='right'>Name</TableCell>
+              <TableCell align='right'>Goal</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {routines &&
+              routines.map((routine) => {
+                return (
+                  <TableRow key={routine.name}>
+                    <TableCell component='th' scope='row'>
+                      {routine.id}
+                    </TableCell>
+                    <TableCell align='right'>{routine.name}</TableCell>
+                    <TableCell align='right'>{routine.goal}</TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
 
 export default Routines;
+
+// import { useState } from "react";
+// import { Button, TextField } from "@material-ui/core";
+// import axios from "axios";
+
+// const Routines = () => {
+//   const [name, setName] = useState();
+//   const [goal, setGoal] = useState();
+//   const [errorMessage, setErrorMessage] = useState();
+
+//   const createRoutine = async () => {
+//     return await axios
+//       .post(`${process.env.REACT_APP_FITNESS_TRACKR_API_URL}routines`, {
+//         name,
+//         goal,
+//       })
+
+//       .then(({ data: { token } }) => {
+//         debugger;
+//         if (token) {
+//           localStorage.setItem("token", JSON.stringify(token));
+//           window.location.href = `${window.location.origin}/Home`;
+//         } else {
+//           setErrorMessage("Something went wrong");
+//         }
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         debugger;
+//       });
+//   };
+//   const onFormSubmit = (event) => {
+//     event.preventDefault();
+//     createRoutine();
+//   };
+
+//   return (
+//     <>
+//       {errorMessage}
+//       <form noValidate autoComplete='off' onSubmit={onFormSubmit}>
+//         <TextField
+//           id='name'
+//           label='Name'
+//           onInput={(event) => {
+//             setName(event.target.value);
+//           }}
+//         />
+//         <TextField
+//           id='goal'
+//           label='goal'
+//           onInput={(event) => {
+//             setGoal(event.target.value);
+//           }}
+//         />
+//         <Button type='submit'>Post</Button>
+//       </form>
+//     </>
+//   );
+// };
+
+// export default Routines;
